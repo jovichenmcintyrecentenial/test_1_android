de07697fbe357e4ff2c442ccaf41bfb10bafa8f9
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-18 17:49:10 -0500

    update main ui

--- app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_test/MainActivity.kt
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_test/MainActivity.kt
@@ -2,10 +2,13 @@ package com.centennial.jovichenmcintyre_mapd711_001_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onSelectRadioButton(view: View) {}
 }
