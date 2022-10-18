d276ca6a964c04bcec4b899b56fa70b8cf5970ae
jovi848 <jovi848.mcintyre@gmail.com>
2022-10-18 18:16:03 -0500

    completed ui and validations

--- app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_test/MainActivity.kt
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_001_test/MainActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.RadioButton
-import android.widget.Toast
+import android.widget.*
 import com.centennial.jovichenmcintyre_mapd711_001_assignment2.exceptions.UserInputException
 import com.google.gson.Gson
 
@@ -16,9 +14,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var  age: EditText
     private lateinit var  weight: EditText
     private lateinit var  height: EditText
+    private lateinit var  spinner: Spinner
 
     private lateinit var gender:String;
     private lateinit var activityLevel:String
+    private lateinit var loadImageCheckbox:CheckBox
     private var loadImage:Boolean = false
 
 
@@ -41,6 +41,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        name = findViewById(R.id.name)
+        age = findViewById(R.id.age)
+        weight = findViewById(R.id.weight)
+        height = findViewById(R.id.height)
+        spinner = findViewById(R.id.spinner)
+        loadImageCheckbox = findViewById(R.id.checkBox)
+
+
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.string_exercise_frequency,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                activityLevel = resources.getStringArray(R.array.string_exercise_frequency)[position]
+            }
+
+        }
     }
 
     fun onSelectRadioButton(view: View) {
@@ -61,6 +91,10 @@ class MainActivity : AppCompatActivity() {
     fun onSubmit(view: View) {
         try{
             if(isDataValid()){
+
+                if (loadImageCheckbox.isChecked) {
+                    loadImage = true
+                }
 
                 val newIntent = Intent(this,OutputActivity::class.java)
                 startActivity(newIntent)
